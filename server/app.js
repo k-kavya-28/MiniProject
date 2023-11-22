acd1f4188ed1a34c2340bcbc6fbd9d65cc8802e3
Abhinav <abhinavgupta560@gmail.com>
2023-11-22 17:13:18 +0530

    backend

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/auth');
+const signupRoutes = require('./routes/signup'); // Import the signup route
 
 const app = express();
 const port = 3000; // or your desired port
@@ -15,6 +16,9 @@ mongoose.connect('mongodb+srv://admin:Mongodb%2311@cluster0.ihowjh3.mongodb.net/
 
 // Use authentication routes
 app.use('/auth', authRoutes);
+
+// Use signup routes
+app.use('../../signup', signupRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
